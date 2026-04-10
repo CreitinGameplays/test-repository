ad83dcc1f2965fe816fb9fea018cc16f201e2aea
Creitin Gameplays <clton999@outlook.com>
2026-04-10 15:25:39 -0300

    cursed code

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define a int
+#define aa main
+#define aaa (
+#define aaaa ) 
+#define aaaaa {
+#define aaaaaa } 
+#define aaaaaaa std::cout
+#define aaaaaaaa << 
+#define aaaaaaaaa "aaaaaaaaa"
+#define aaaaaaaaaa std::endl
+#define aaaaaaaaaaa ;
+#define aaaaaaaaaaaa return
+#define aaaaaaaaaaaaa 0
+
+a aa aaa aaaa aaaaa
+    aaaaaaa aaaaaaaa aaaaaaaaa aaaaaaaa aaaaaaaaaa aaaaaaaaaaa
+    aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaa
+aaaaaa
