d5fda5c33915fc1e6405b58585b94f5c8e27fc16
Creitin Gameplays <clton999@outlook.com>
2026-04-10 16:07:31 -0300

    remove

--- src_cpp/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using string = std::string;
-int main() {
-  string name {};
-  std::cout << "Simple test code\n";
-  std::getline(cin, string);
-  std::cout << "You said: " << name << '\n';
-  return 0;
-}
