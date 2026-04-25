a5b379d85fe13d966115eddff772ff164a6ac329
Creitin Gameplays <clton999@outlook.com>
2026-04-24 22:17:24 -0300

    virus update

--- src/virus.cc
+++ src/virus.cc
@@ -1,8 +1,9 @@
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
 // main virus
 int main(){
-    ShowWindow(GetConsoleWindow(), SW_HIDE); // ouch we can't hijack the user, we fail
+    ShowWindow(GetConsoleWindow(), SW_HIDE); // ouch we can't hijack the user, so we fail
     const char* text = "Hi, I am Azerbaijan virus, due to poor "
     "technology in my country I am not able "
     "to damage your computer, please be "
@@ -10,5 +11,5 @@ int main(){
     "computer with a hammer";
 
     MessageBox(NULL, text, "Virus Alert !", MB_OK | MB_ICONERROR);
-    return 1; // its an error
+    return 0;
 }
