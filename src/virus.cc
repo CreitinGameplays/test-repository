a857f373107265a9e1eae48966971bf6077ae5e7
Creitin Gameplays <clton999@outlook.com>
2026-04-24 21:57:48 -0300

    ahhhh

--- src/virus.cc
+++ src/virus.cc
@@ -4,7 +4,7 @@
 int main(){
     ShowWindow(GetConsoleWindow(), SW_HIDE); // ouch we can't hijack the user, we fail
     const char* text = "Hi, I am Azerbaijan virus, due to poor "
-    "technology in my country I not able "
+    "technology in my country I am not able "
     "to damage your computer, please be "
     "kind enough to smash your "
     "computer with a hammer";
