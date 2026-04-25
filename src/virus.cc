d3db37e139fb4b19c1a8c5f988f8a3b2336f3b98
Creitin Gameplays <clton999@outlook.com>
2026-04-24 22:08:54 -0300

    aaaaaaaaaaaaaaa

--- src/virus.cc
+++ src/virus.cc
@@ -8,7 +8,7 @@ int main(){
     "to damage your computer, please be "
     "kind enough to smash your "
     "computer with a hammer";
-    // window
-    MessageBox(NULL, text, "Azerbaijan virus", MB_OK);
-    return 0;
+
+    MessageBox(NULL, text, "Azerbaijan virus", MB_OK | MB_ICONERROR);
+    return 1; // its an error
 }
