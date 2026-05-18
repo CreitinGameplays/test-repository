62b4339bc4cb8bd07947056fdf8fed8d5f80fbd3
Creitin Gameplays <clton999@outlook.com>
2026-05-18 18:43:30 -0300

    the fuck

--- real/main.cc
+++ real/main.cc
@@ -3,13 +3,15 @@
 #include <string>
 
 // declaration
-std::string readFile(std::string file);
-
 void parseInfo(std::string jsonstr){
     size_t findName = jsonstr.find("latest_stable");
+    if (findName == std::string::npos) throw std::runtime_error("Could not find latest_stable.\n");
     size_t findVersion = jsonstr.find("version", findName);
+    if (findVersion == std::string::npos) throw std::runtime_error("Could not find version.\n");
     size_t findColon = jsonstr.find(':', findVersion);
+    if (findColon == std::string::npos) throw std::runtime_error("Failure on findColon.\n");
     size_t findQuot = jsonstr.find('\"', findColon);
+    if (findQuot == std::string::npos) throw std::runtime_error("Failure on findQuot.\n");
 
     int start = findQuot + 1;
     std::string finalValue;
@@ -21,46 +23,43 @@ void parseInfo(std::string jsonstr){
     std::cout << "Latest stable linux kernel version: " << finalValue << "\n";
 }
 
+std::string cmd(std::string command){
+    char b[256]; // the buffer
+    std::string final_output = "";
+    FILE* file = popen(command.c_str(), "r"); // "r" = read
+    if (!file){
+        throw std::runtime_error("Insane error on cmd()! Popen error.\n"); 
+    }
+    while(fgets(b, sizeof(b), file) != NULL){
+        final_output += b; // append to the variable
+    }
+    pclose(file);
+    return final_output;
+}
+
 // get data
 namespace linuxInfo{
     int returnData(){
-        // lazy download
+        // lazy curl
         #ifdef _WIN32
-        std::system("curl -O https://www.kernel.org/releases.json >nul 2>&1"); 
+        std::string command = cmd("curl.exe -sS https://www.kernel.org/releases.json");
         #else defined(__linux__)
-        std::system("curl -O https://www.kernel.org/releases.json >/dev/null 2>&1");
+        std::string command = cmd("curl -sS https://www.kernel.org/releases.json");
         #endif
-
-        std::string file = readFile("releases.json");
-        parseInfo(file);
+        parseInfo(command);
         return 0;
     }
 };
 
 // main function
 int main(void){
-    linuxInfo::returnData();
+    try{
+        linuxInfo::returnData();
+    } catch (const std::exception &e){
+        std::cerr << "Error: " << e.what() << "\n";
+        return EXIT_FAILURE;
+    }
     return 0;
-}
-
-std::string readFile(std::string file){
-    // open the goddamn file
-    std::ifstream jsonfile(file);
-    if(!jsonfile.is_open()){
-        std::cerr << "wild file reading error\n";
-    }
-
-    std::string cur_line;
-    std::string jsonStr;
-    // extract each line
-    while (std::getline(jsonfile, cur_line)){
-        jsonStr += cur_line + '\n';
-    }
-
-    // cleanup
-    jsonfile.close();
-    std::remove("releases.json");
-    return jsonStr;
 }
 
 // most useless program ever
